94e7368e0995aec54139c2656f96838cde902eb3
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2025-08-05 14:55:04 -0500

    Changes Files - Archives

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+  import "../styles/style.css";
+  let link = "https://www.google.com/";
+</script>
+
+<main>
+  <center>
+    <header>
+      <h1>blog-jct - JeanCarlos Jimenez</h1>
+      <br />
+      <small>Desarrollador Web - Frontend Junior</small>
+      <br />
+      <strong
+        >Esta Web esta Creada con <a id="a-strong" href="http://">svelte</a> y
+        <a id="a-strong" href="http://">vite</a>
+        y theme - <a id="a-strong" href="http://">rose pine</a></strong
+      >
+    </header>
+    <nav>
+      <ul>
+        <li><a href={link}>Inicio</a></li>
+        <li><a href={link}>Sobre mi</a></li>
+        <li><a href={link}>Mis Blogs</a></li>
+        <li><a href={link}>Mis Stacks</a></li>
+        <li><a href={link}>Mis Proyectos</a></li>
+      </ul>
+    </nav>
+  </center>
+</main>
+
+<style></style>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-//Todo - https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+	plugins: [sveltekit()]
 });
